a904094e0f30e38f4f22fbbe2409fd489f736539
Eze <Mailsforeze@gmail.com>
2019-08-27 15:00:29 +0100

    adds SideBar, TopMenu, and Dashboard components

---
+++ src/views/Sidebar/index.vue
@@ -0,0 +1,73 @@
+<template>
+    	<!-- Sidebar -->
+            <div class="sidebar" id="sidebar">
+                <div class="sidebar-inner slimscroll">
+					<div id="sidebar-menu" class="sidebar-menu">
+						<ul>
+							<li class="active"> 
+								<a href="index.html"><i class="la la-dashboard"></i> <span>Dashboard</span></a>
+							</li>
+							<li class="submenu">
+								<a href="#" class="noti-dot"><i class="la la-user"></i> <span> Employees</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="employees.html">All Employees</a></li>
+									<li><a href="holidays.html">Holidays</a></li>
+									<li><a href="leaves.html">Leaves (Admin) <span class="badge badge-pill bg-primary float-right">1</span></a></li>
+									<li><a href="leaves-employee.html">Leaves (Employee)</a></li>
+									<li><a href="leave-settings.html">Leave Settings</a></li>
+									<li><a href="attendance.html">Attendance (Admin)</a></li>
+									<li><a href="attendance-employee.html">Attendance (Employee)</a></li>
+									<li><a href="departments.html">Departments</a></li>
+									<li><a href="designations.html">Designations</a></li>
+									<li><a href="timesheet.html">Timesheet</a></li>
+									<li><a href="promotion.html">Promotion</a></li>
+									<li><a href="resignation.html">Resignation</a></li>
+									<li><a href="termination.html">Termination</a></li>
+									<li><a href="overtime.html">Overtime</a></li>
+								</ul>
+							</li>
+							<li class="submenu">
+								<a href="#"><i class="la la-money"></i> <span> Payroll </span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="salary.html"> Employee Salary </a></li>
+									<li><a href="salary-view.html"> Payslip </a></li>
+									<li><a href="payroll-items.html"> Payroll Items </a></li>
+								</ul>
+							</li>
+							<li class="submenu">
+								<a href="#"><i class="la la-building"></i> <span> Jobs </span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="jobs.html"> Manage Jobs </a></li>
+									<li><a href="job-applicants.html"> Applied Candidates </a></li>
+								</ul>
+							</li>
+							<li> 
+								<a href="tickets.html"><i class="la la-ticket"></i> <span>Tickets</span></a>
+							</li>
+						
+							<li> 
+								<a href="policies.html"><i class="la la-file-pdf-o"></i> <span>Policies</span></a>
+							</li>
+						
+							<li class="submenu">
+								<a href="#"><i class="la la-pie-chart"></i> <span> Reports </span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="expense-reports.html"> Expense Report </a></li>
+									<li><a href="invoice-reports.html"> Invoice Report </a></li>
+								</ul>
+							</li>
+							<li> 
+								<a href="settings.html"><i class="la la-cog"></i> <span>Settings</span></a>
+							</li>
+						</ul>
+					</div>
+                </div>
+            </div>
+			<!-- /Sidebar -->
+</template>
+
+<script>
+export default {
+    name:"SideBar"
+}
+</script>
